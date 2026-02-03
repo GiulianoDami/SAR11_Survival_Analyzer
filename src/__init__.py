b38114c3e3c3aa560ca45974d267c3962d061e41
GiulianoDami <38913937+GiulianoDami@users.noreply.github.com>
2026-02-03 09:59:10 +0100

    Code fix

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from .model import SAR11Model
+from .stressor import EnvironmentalStressor
+from .visualization import plot_population_trends, plot_cell_size_distribution
+
+__all__ = ['SAR11Model', 'EnvironmentalStressor', 'plot_population_trends', 'plot_cell_size_distribution']
